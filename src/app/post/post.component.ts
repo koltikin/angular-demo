f239ab2c2f2b1f5c440457053a73f783076aeb4b
Alim <koltikin210@gmail.com>
2024-05-26 14:55:51 +0300

    oservable

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../shared/post';
 import { PostService } from '../post.service';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-post',
@@ -16,4 +17,16 @@ export class PostComponent implements OnInit {
     this.postService.getPosts$().subscribe(
       response => this.posts = response)
   }
+
+  numbers: Observable<number> = of(1,2,3,4,5,6,7,8,9,10);
+
+  words: Observable<string> = new Observable<string>(
+    (observer) =>{
+      observer.next("hello");
+      observer.next("world");
+      setTimeout(() => {
+        observer.next("final")
+      },2000);
+    }
+  );
 }
